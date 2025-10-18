65bf73d965e1ab443af9e5cd0114fd13f2cd1c9f
AnabelMarinho <anabel.soares2001@gmail.com>
2025-10-18 20:17:27 -0300

    Initial commit

---
+++ src/main/java/ufersa/dev/ApiFinanca/ApiFinancaApplication.java
@@ -0,0 +1,13 @@
+package ufersa.dev.ApiFinanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufersa/dev/ApiFinanca/ApiFinancaApplicationTests.java
@@ -0,0 +1,13 @@
+package ufersa.dev.ApiFinanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
