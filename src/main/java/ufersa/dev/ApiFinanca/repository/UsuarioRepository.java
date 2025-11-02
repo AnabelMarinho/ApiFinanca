c5a9e440b0db5a7149175ddf2aaf4fa2bc78f474
AnabelMarinho <anabel.soares2001@gmail.com>
2025-11-02 09:53:58 -0300

    feat: adiciona model de usuário

---
+++ src/main/java/ufersa/dev/ApiFinanca/repository/UsuarioRepository.java
@@ -0,0 +1,66 @@
+package ufersa.dev.ApiFinanca.repository;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "usuarios")
+public class UsuarioRepository {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private BigDecimal faixaSalario;
+
+    public UsuarioRepository(Long id, String nome, String email, String senha, BigDecimal faixaSalario) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.faixaSalario = faixaSalario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public BigDecimal getFaixaSalario() {
+        return faixaSalario;
+    }
+
+    public void setFaixaSalario(BigDecimal faixaSalario) {
+        this.faixaSalario = faixaSalario;
+    }
+}
---
+++ src/main/java/ufersa/dev/ApiFinanca/service/UsuarioService.java
@@ -0,0 +1,4 @@
+package ufersa.dev.ApiFinanca.service;
+
+public class UsuarioService {
+}
