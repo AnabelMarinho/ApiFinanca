739fa2464e18afaeed71adc1c2fd9beb604272ba
Nicolas Emanuel <niculas20003@outlook.com>
2025-11-05 10:44:43 -0300

    feat: adicionado model de Categoria

---
+++ src/main/java/ufersa/dev/ApiFinanca/model/Categoria.java
@@ -0,0 +1,30 @@
+package ufersa.dev.ApiFinanca.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoTransacao tipo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Usuario user;
+
+}
