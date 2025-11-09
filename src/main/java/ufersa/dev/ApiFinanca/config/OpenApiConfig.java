7b74be39fbbfc8ca45ae2aeafb77b63bfe65c137
Nicolas Emanuel <niculas20003@outlook.com>
2025-11-09 10:55:01 -0300

    feat: adiciona swagger no sistema pra testar os endpoints

---
+++ src/main/java/ufersa/dev/ApiFinanca/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package ufersa.dev.ApiFinanca.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Finança",
+                version = "1.0",
+                description = "Documentação da API de finanças pessoais"
+        )
+)
+public class OpenApiConfig {
+}
+
+
