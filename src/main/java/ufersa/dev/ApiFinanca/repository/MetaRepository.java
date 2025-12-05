56be5ac776b1944e0e7cc7e3ec303f70d38cffaf
AnabelMarinho <anabel.soares2001@gmail.com>
2025-12-04 21:52:30 -0300

    feat: criação de MetaRepository e AporteMetaRepository

---
+++ src/main/java/ufersa/dev/ApiFinanca/repository/MetaRepository.java
@@ -0,0 +1,13 @@
+package ufersa.dev.ApiFinanca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ufersa.dev.ApiFinanca.model.Meta;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MetaRepository extends JpaRepository<Meta, UUID> {
+    List<Meta> findByUsuarioId(UUID id);
+}
